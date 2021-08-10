37dfa0d9286c65a66944428c029f8a35b307c5f0
Gabriel Ribeiro <gabriel@enzo.com.br>
2021-08-09 23:01:13 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-$('.owl-carousel').owlCarousel({
-    items: 2,
+$('.owl-carousel.filmes-principais').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    dots: true,
     autoWidth:true,
     autoplay:true,
     autoplayTimeout:3000,
     autoplayHoverPause:true,
+    lazyLoad: true,
     responsive:{
         0:{
             items:1
@@ -16,6 +17,25 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items:5
+        }
+    }
+})
+
+$('.owl-carousel.filmes-secundarios').owlCarousel({
+    items: 7,
+    loop:false,
+    nav:true,
+    lazyLoad: true,
+    margin: 15,
+    responsive:{
+        0:{
+            items:3
+        },
+        100:{
+            items:5
+        },
+        1000:{
+            items:7
         }
     }
 })
